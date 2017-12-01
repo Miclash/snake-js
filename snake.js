b492feac52539a7a5a28cc62e7f6b5bd73b3d49e
Michal Lasak <michal.lasak@gmail.com>
2017-12-01 08:06:22 +0100

    asdf

--- snake.js
+++ snake.js
@@ -1,26 +1,55 @@
 'use strict'
  
-var AllPos = [];
-AllPos[0] = [0,0];
-AllPos[1] = [1,0];
-AllPos[2] = [2,0];
-AllPos[3] = [3,0];
-AllPos[4] = [4,0];
+var Fruits = [];
 
+
+var AllPos = [];
+
+
+var Speed = 10;
 var Xdir = 1;
 var Ydir = 0;
 
-var Speed = 10;
 
 var canvas = document.getElementById('canvas1');
 var ctx = canvas.getContext('2d');
- 
-ctx.fillStyle = 'blue';
-ctx.fillRect(0, 0, 300, 300);
- 
 var myVar = setInterval(myMove, 100);
+
+document.addEventListener('keydown', KeyPress, true);
+
+RunGame();
+
+function RunGame() {
+    Speed = 10;
+    Xdir = 1;
+    Ydir = 0;
+
+    AllPos = [];
+    AllPos[0] = [0,0];
+    AllPos[1] = [1,0];
+    AllPos[2] = [2,0];
+    AllPos[3] = [3,0];
+    AllPos[4] = [4,0];
+
+    Fruits = [];
+    Fruits[0] = [40,50];
+    Fruits[1] = [100,70];
+    Fruits[2] = [200, 100];
+
     
-document.addEventListener('keydown', KeyPress, false);
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(0, 0, 300, 300);
+    
+
+
+    ctx.fillStyle = 'green';
+    for (var n = 0; n < Fruits.length; n++){
+        ctx.fillRect(Fruits[n][0]  * Speed , Fruits[n][1] * Speed,10,10);
+    }
+
+    return ;
+}
+
 
 function myMove() {
     var l = AllPos.length
@@ -28,22 +57,37 @@ function myMove() {
 
         ctx.fillStyle = 'blue';
         ctx.fillRect(AllPos[0][0]  * Speed , AllPos[0][1] * Speed,10,10);
+        ctx.fillStyle = 'green';
+        for (var n = 0; n < Fruits.length; n++){
+            ctx.fillRect(Fruits[n][0]   , Fruits[n][1] ,10,10);
+        }
+
         ctx.fillStyle = 'white';
         AllPos[l-1][0] = AllPos[l-1][0] + Xdir;
         AllPos[l-1][1] = AllPos[l-1][1] + Ydir;
 
 
         var imgData=ctx.getImageData(AllPos[l-1][0] * Speed , AllPos[l-1][1] * Speed,10,10).data;
-        if (IsCollision(imgData)){
+        if (IsCollision(imgData, 0, 0, 0)){
             document.removeEventListener('keydown', KeyPress)
             ctx.fillStyle = 'red';
             ctx.fillRect(0,0,300,300);
             return ;
         }
 
+        var EncounterFruit = 0;
+        if (IsCollision(imgData, 0,255,0)){
+            EncounterFruit =1;
+        }
+        if (IsCollision(imgData, 0,128,0)){
+            EncounterFruit =1;
+        }
         ctx.fillRect(AllPos[l-1][0] * Speed , AllPos[l-1][1] * Speed,10,10);
     
-
+        if (EncounterFruit != 0)
+        {
+            stop;
+        }
 
         for(var j=0;   j < l-1; j++)
         {
@@ -56,22 +100,26 @@ function myMove() {
         
 }
 
-function IsCollision(imgData) {
+function IsCollision(imgData, red, green, blue) {
 
-    return imgData[0] == 0 && imgData[1] == 0 && imgData[2] == 0;
-    for (var i = 0, n = pix.length; i < n; i += 4) {
-        pix[i  ] = 255 - pix[i  ]; // red
-        pix[i+1] = 255 - pix[i+1]; // green
-        pix[i+2] = 255 - pix[i+2]; // blue
-        // i+3 is alpha (the fourth element)
-    }
     
+     for (var i = 0, n = imgData.length; i < n; i += 4) {
+         if (imgData[i+0] == red && imgData[i+1] == green && imgData[i+2] == blue)
+         {
+             return true;
+         }
+    //     pix[i  ] = 255 - pix[i  ]; // red
+    //     pix[i+1] = 255 - pix[i+1]; // green
+    //     pix[i+2] = 255 - pix[i+2]; // blue
+    //     // i+3 is alpha (the fourth element)
+     }
+    return false;
 }
-
+ 
 function KeyPress(e)
 {
     var keyCode = e.keyCode;
-    //console.log(keyCode);
+    console.log(keyCode);
     switch(keyCode){
         case 37:
         Xdir = -1; Ydir = 0;
@@ -84,6 +132,9 @@ function KeyPress(e)
         break;
         case 40:
         Xdir = 0; Ydir = 1;
+        break;
+        case 32:
+        RunGame();
         break;
     }
 }
